37db48ee87ba29ff21c38b5accd167a027bd699f
omarion3698 <omaribinbakarivic@gmail.com>
2020-10-13 19:54:38 +0300

    Rename files

--- pictures/views.py
+++ pictures/views.py
@@ -12,7 +12,7 @@ def index(request):
 def image_location(request, location):
     images = Image.filter_by_location(location)
     print(images)
-    return render(request, 'pictures/index.html', {'location_images': images})
+    return render(request, 'pictures/location.html', {'location_images': images})
 
 
 def search_results(request):
@@ -21,7 +21,7 @@ def search_results(request):
         searched_images = Image.search_by_category(category)
         message = f"{category}"
         print(searched_images)
-        return render(request, 'pictures/search.html', {"message": message, "images": searched_images})
+        return render(request, 'pictures/search_results.html', {"message": message, "images": searched_images})
     else:
         message = "You haven't searched for any image category"
-        return render(request, 'pictures/search.html', {"message": message})
+        return render(request, 'pictures/search_results.html', {"message": message})
